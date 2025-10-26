7c24f19caa125b8384e207fbf8f4e64188b62c81
heisraymond <imraymond07@gmail.com>
2025-10-26 13:06:21 +0300

    Added Diceroller class which is a stateful widget

--- workspace/diceapp/lib/gradient_container.dart
+++ workspace/diceapp/lib/gradient_container.dart
@@ -6,13 +6,20 @@ import 'package:flutter/material.dart';
 var startAlignment = Alignment.topLeft;
 var endAlignment = Alignment.bottomRight;
 
+// ignore: must_be_immutable
 class GradientContainer extends StatelessWidget {
   // Constructor function
   // Let's add named positional for colors
-  const GradientContainer(this.colors, {super.key});
+  GradientContainer(this.colors, {super.key});
 
   // Class variables
   final List<Color> colors;
+  var activeDiceImage = 'assets/images/dice-2.png';
+
+  void rollDice() {
+    activeDiceImage = 'assets/images/dice-3.png';
+    print('Changing image');
+  }
 
   @override
   // Returns a widget
@@ -32,7 +39,31 @@ class GradientContainer extends StatelessWidget {
           // If we want the value to be passed to the StyledText()
           // class, then we have to define the value as an argument
           // in it's constructor function
-          child: Image.asset('assets/images/dice-one.png')
+          child: Column(
+            //By default main axis size is maximum (stays at the top)
+            //But we use minimum, so as the dice can stay at the center
+            mainAxisSize: MainAxisSize.min,
+            children: [
+             Image.asset(
+              activeDiceImage, 
+              width: 200,
+             ),
+             const SizedBox(height: 20,),
+             TextButton(
+              onPressed: rollDice, 
+              //Just a convention, style first
+              //then follows widget arguments
+              style: TextButton.styleFrom(
+                foregroundColor: const Color.fromARGB(255, 254, 245, 215),
+                textStyle: const TextStyle(
+                  inherit: true,
+                  fontSize: 28,
+                )
+              ),
+              child: const Text('Roll Dice')
+              )
+            ],
+          ) 
           ),
       );
   }
